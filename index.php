9586ec1846f14195a79ff68be602f474c3eb95af
venkat5947 <75569977+venkat5947@users.noreply.github.com>
2021-06-02 14:52:18 +0530

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php  
+session_start(); 
+
+include("head.php");
+// include("DataSource.php");
+// $db=new DataSource();
+// include('user.php');
+// $que = new user();
+
+?>
+
+
+
+
+
+<?php include("tail.php"); ?>
+
+<script type="text/javascript" language="javascript">
+        jQuery(document).ready(function(){
+                
+                  
+             
+            
+        })
+</script>
